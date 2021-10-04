b35663434238473463ec0712d329173139e173f3
Victor Shinkevich <supostatekx@gmail.com>
2021-10-04 20:09:43 +0300

    Use dto for update task status

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -3,6 +3,7 @@ import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 
 @Injectable()
 export class TasksService {
@@ -56,8 +57,9 @@ export class TasksService {
     return task;
   }
 
-  updateTaskStatus(id: string, status: TaskStatus): Task {
+  updateTaskStatus(id: string, updateTaskStatusDto: UpdateTaskStatusDto): Task {
     const task: Task = this.getTaskById(id);
+    const { status } = updateTaskStatusDto;
     task.status = status;
     return task;
   }
